e24f6d4cf990c457e39028d1e81d0169605e3716
Nadia Ahmed Saleh <71907566+NadiaAhmedSaleh@users.noreply.github.com>
2025-04-22 01:24:59 +0200

    initialized vite + react todolist

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { StrictMode } from 'react';
+import { Provider } from '@/components/ui/provider'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+<Provider>
+      <App />
+    </Provider>
+
+
+  </StrictMode>
+)
