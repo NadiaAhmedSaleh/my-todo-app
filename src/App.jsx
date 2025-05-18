609c4ea1d4693ed40f60eb2c011f84730beb2dd9
Nadia Ahmed Saleh <71907566+NadiaAhmedSaleh@users.noreply.github.com>
2025-05-18 16:57:38 +0300

    add and delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,93 @@
 import './App.css'
-
+import { Box , Center, Heading} from '@chakra-ui/react'
+import { Input, Stack } from "@chakra-ui/react"
+import { useColorMode, useColorModeValue } from "@/components/ui/color-mode"
+import { ColorModeButton } from "@/components/ui/color-mode"
+import { Button } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
+import { useState } from 'react'
+import { CloseButton } from "@chakra-ui/react"
 
 
 
 function App() {
 
+  const { toggleColorMode } = useColorMode()
+
+  const bg = useColorModeValue("#F4F4F5" , "#121212")
+  const color = useColorModeValue("gray.800" ,"white")
+  
+  
+  const [task , setTask] = useState('');
+  const [tasks , setTasks] = useState([]);
+
+
+  //Add
+
+  const addTask = ()=>{
+    if (task.trim()!==''){
+      setTasks([...tasks, task]);
+      setTask('')
+    }
+  }
+
+  //Delete
+  
+  const deleteTask = (index)=>{
+    const newTasks = [...tasks.slice(0,index), ...tasks.slice(index+1)];
+    setTasks(newTasks);
+  }
   return (
     <>
-   <h1>hello</h1>
+  
 
+  <Center>
+   <Stack align="flex-start" gap="4">
+       <ColorModeButton onClick={toggleColorMode} />
+        
+        <Heading color={color} letterSpacing={".5em"}>ToDo</Heading>
+
+     
+    <Box p="2" bg={bg} width={"70vw"} minH="70vh" color={color} >
+      <Flex gap="4" >
+      <Input placeholder="Add a Task" variant="outline"  width="400px"
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+      onKeyDown={(e) => e.key === 'Enter' && addTask()}
+      />
+      <Button variant="surface" onClick={addTask}>Add</Button>
+      </Flex>  
+
+
+
+
+      <Flex gap="4" >
+
+    <ul>
+      {tasks.map((task , index)=> (
+        <Box as="li" _marker={{ color: "red" }} key={index}>
+        {task}
+        <CloseButton  onClick={() => deleteTask(index)} />
+      </Box>
+    
+      ))}
+  
+   </ul>
+     
+      
+      </Flex> 
+
+
+
+      </Box>
+
+
+      
+    </Stack>
+    </Center>
+
+  
+  
  
   
     </>
